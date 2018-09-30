dd480050ace7d3d547c2f8187d5042e1cd8f3afd
heppoko9 <heppoko9@gmail.com>
2018-09-30 22:32:24 +0900

    add Bridging-Header.h

---
+++ Google Analytics/Bridging-Header.h
@@ -0,0 +1,18 @@
+//
+//  Bridging-Header.h
+//  Google Analytics
+//
+//  Created by 中村昭 on 2018/09/23.
+//  Copyright © 2018年 中村昭. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+/// GoogleAnalytics
+#import <GoogleAnalytics/GAI.h>
+#import <GoogleAnalytics/GAIFields.h>
+#import <GoogleAnalytics/GAIDictionaryBuilder.h>
+#import <GoogleAnalytics/GAIEcommerceFields.h>
+
+#endif /* Bridging_Header_h */
